26710e4bc1db42776e472da105529fa8cd5707df
Bhair-v <2220678@iub.edu.bd>
2025-04-20 11:19:23 +0600

    s

---
+++ src/main/java/com/example/maricobd/Login.java
@@ -0,0 +1,23 @@
+package com.example.maricobd;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginFXML.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/com/example/maricobd/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.maricobd;
+
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class LoginController {
+
+    @javafx.fxml.FXML
+    private void handleFinancialExecLogin(ActionEvent event) throws IOException {
+
+        
+    }
+}
